6e118487ec79076df436b94128423d4da51877fa
Muhammad Risjad Shidqi Febian <mrisjads@gmail.com>
2023-04-02 19:23:42 +0700

    menambahkan class KuliahMahasiswa_1302213045

---
+++ KuliahMahasiswa_1302213045.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMODUL7_kelompokEliteGlobal_6
+{
+    internal class KuliahMahasiswa_1302213045
+    {
+        public void ReadJSON()
+        {
+            string JSONString = File.ReadAllText(@"C:\Users\mrisj\Documents\TELKOM\Telkom University\Semester 4\Praktikum KPL\MODUL 7\TPMODUL7_kelompokEliteGlobal_6\tp7_2_1302213045.json");
+            dynamic JSONObject = JsonConvert.DeserializeObject<dynamic>(JSONString);
+
+            int i = 0;
+            Console.WriteLine("Daftar Matakuliah : ");
+            foreach (var course in JSONObject.courses)
+            {
+                Console.WriteLine($"MataKuliah : {i} {course.code} - {course.name}");
+                i++;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
 ﻿using TPMODUL7_kelompokEliteGlobal_6;
 
-Console.WriteLine("Contoh Implementasi READ JSON");
+Console.WriteLine("Contoh Implementasi READ JSON No 1. \n");
 
 DataMahasiswa1302213045 soal1 = new DataMahasiswa1302213045();
 soal1.READJSON();
+
+Console.WriteLine("\nContoh Implentasi READ JSON No 2. \n");
+
+KuliahMahasiswa_1302213045 soal2 = new KuliahMahasiswa_1302213045();
+soal2.ReadJSON();
