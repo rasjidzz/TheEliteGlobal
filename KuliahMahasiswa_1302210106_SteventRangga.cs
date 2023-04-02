f972e82729ba72202cd27ea30769e5743ca9e2d4
Law1911 <epensusanto@gmail.com>
2023-04-02 19:09:59 +0700

    menambahkan class KuliahMahasiswa_1302210106_SteventRangga

---
+++ KuliahMahasiswa_1302210106_SteventRangga.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Text.Json.Nodes;
+
+namespace TPMODUL7_kelompokEliteGlobal_6
+{
+    internal class KuliahMahasiswa_1302210106_SteventRangga
+    {
+        public void ReadJson()
+        {
+            string json = File.ReadAllText(@"D:\Coding\C#\TP_MOD7\TheEliteGlobal\tp7_2_1302210106.json");
+
+            dynamic jsonc = JsonConvert.DeserializeObject(json);
+
+            Console.WriteLine("Daftar mata kuliah yang diambil:");
+            int i = 1;
+            foreach (var course in jsonc.courses)
+            {
+                Console.WriteLine($"MK {i} {course.code}-{course.name}");
+                i++;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,3 +2,6 @@
 
 DataMahasiswa_1302210106_SteventRangga a = new DataMahasiswa_1302210106_SteventRangga();
 a.ReadJSON();
+
+KuliahMahasiswa_1302210106_SteventRangga b = new KuliahMahasiswa_1302210106_SteventRangga();
+b.ReadJson();
