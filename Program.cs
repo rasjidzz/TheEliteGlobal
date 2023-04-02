ed9914b84a591a0f803e6337836985515cb0ff17
logicrius <113976617+logicrius@users.noreply.github.com>
2023-04-02 20:12:21 +0700

    DataMahasiswa_1302210070_Ashar_Syahbudi_S_ a = new DataMahasiswa_1302210070_Ashar_Syahbudi_S_

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using TPMODUL7_kelompokEliteGlobal_6;
 
+DataMahasiswa_1302210070_Ashar_Syahbudi_S_ a = new DataMahasiswa_1302210070_Ashar_Syahbudi_S_ ();
+a.ReadJSON();
+
 KuliahMahasiswa_1302210070_AsharSyahbudiS b = new KuliahMahasiswa_1302210070_AsharSyahbudiS();
 b.ReadJSON();
