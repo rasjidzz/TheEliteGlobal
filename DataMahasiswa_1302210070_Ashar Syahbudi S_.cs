88b86e87a719734a95095f16f4c94f0ed2a4970a
logicrius <113976617+logicrius@users.noreply.github.com>
2023-04-02 17:15:46 +0700

    Menambahkan Class DataMahasiswa_1302210070_Ashar_Syahbudi_S_
    Menambahkan Class DataMahasiswa_1302210070_Ashar_Syahbudi_S_

---
+++ DataMahasiswa_1302210070_Ashar Syahbudi S_.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TPMODUL7_kelompokEliteGlobal_6
+{
+    internal class DataMahasiswa_1302210070_Ashar_Syahbudi_S_
+    {
+        public void ReadJSON()
+        {
+
+            string jsonString = File.ReadAllText(@"D:\TELKOM\Pembelajaran\Semester 4\KPL\TP\clone repo\TheEliteGlobal\tp7_1_1302210070.json");
+
+            dynamic jsonObject = JsonConvert.DeserializeObject(jsonString);
+
+            Console.WriteLine($"Nama {jsonObject.nama.depan} {jsonObject.nama.belakang} dengan nim {jsonObject.nim} dari fakultas {jsonObject.fakultas}");
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Testing ini commit awal aja ges");
+﻿using TPMODUL7_kelompokEliteGlobal_6;
+
+DataMahasiswa_1302210070_Ashar_Syahbudi_S_ a = new DataMahasiswa_1302210070_Ashar_Syahbudi_S_ ();
+a.ReadJSON();
